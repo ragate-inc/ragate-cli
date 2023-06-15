dcc188e4cab8c5861302a07c619572b1ead8cb00
m-sugiyama-ragate <m.sugiyama@ragate.co.jp>
2023-06-15 18:30:22 +0900

    fix path

--- lib/features/codegen/features/crud/index.ts
+++ lib/features/codegen/features/crud/index.ts
@@ -1,5 +1,5 @@
-import builder from 'features/add/features/sns/builder';
-import handler from 'features/add/features/sns/handler';
+import builder from 'features/codegen/features/crud/builder';
+import handler from 'features/codegen/features/crud/handler';
 export default {
   builder,
   handler,
